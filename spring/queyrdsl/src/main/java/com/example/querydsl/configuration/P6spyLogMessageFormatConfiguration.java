9388719d7e3b3c195f1b3de4680805bf35f5dd73
임용태 <yt.yim@newploy.net>
2020-11-13 09:32:34 +0900

    p6spy 로깅 포멧 적용

---
+++ spring/queyrdsl/src/main/java/com/example/querydsl/configuration/P6spyLogMessageFormatConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.querydsl.configuration;
+
+import com.p6spy.engine.spy.P6SpyOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class P6spyLogMessageFormatConfiguration {
+
+    @PostConstruct
+    public void setLogMessageFormat() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spySqlFormatConfiguration.class.getName());
+    }
+}
